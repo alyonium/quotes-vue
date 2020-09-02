c95e6f2899f30766b43f7fe495c860d4efa618c4
Alona Ivanova <alyonium2.0@gmail.com>
2020-09-02 23:45:45 +0300

    app

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <label>Quote</label>
+                <textarea name="" id="" cols="80" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+                <button class="btn btn-primary" @click.prevent="createNew">Add quote</button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    label {
+        width: 100%
+    }
+
+
+</style>
